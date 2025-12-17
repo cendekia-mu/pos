84c5a9ae653ca8b0f1b2d16248e53575bf4d9590
aliefmrizal-oss <aliefmrizal@gmail.com>
2025-12-17 12:41:09 +0700

    update invoice

--- tsa_pos/views/kecamatan.py
+++ tsa_pos/views/kecamatan.py
@@ -7,18 +7,18 @@ from ..i18n import _
 
 class ListSchema(colander.Schema):
     id = colander.SchemaNode(colander.Integer(),
-                             missing=colander.drop,
-                             title="Action",
-                             widget=widget.HiddenWidget())
+                                missing=colander.drop,
+                                title="Action",
+                                widget=widget.HiddenWidget())
     name = colander.SchemaNode(colander.String())
 
 
 class CreateSchema(colander.Schema):
     # Define your schema fields here
     name = colander.SchemaNode(colander.String(),
-                               validator=colander.Length(min=3, max=50))
+                                validator=colander.Length(min=3, max=50))
     kota_id = colander.SchemaNode(colander.Integer(),
-                                      widget=widget.SelectWidget(values=[]),)   
+                                widget=widget.SelectWidget(values=[]),)   
 
     def after_bind(self, schema, appstruct):
         # Populate category_id choices
@@ -29,10 +29,10 @@ class CreateSchema(colander.Schema):
 
 class UpdateSchema(CreateSchema):
     id = colander.SchemaNode(colander.Integer(),
-                             missing=colander.drop,
-                             widget=widget.HiddenWidget())
+                                missing=colander.drop,
+                                widget=widget.HiddenWidget())
     kota_id = colander.SchemaNode(colander.Integer(),
-                                      widget=widget.SelectWidget(values=[]),)   
+                                widget=widget.SelectWidget(values=[]),)   
 
     def after_bind(self, schema, appstruct):
         # Populate category_id choices
