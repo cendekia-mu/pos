55d395b44cc9004c3fbd15f5b2ab8112cd5154c8
Dzakwan123 <182151108@student.unsil.ac.id>
2025-12-18 09:57:07 +0700

    base views

--- tsa_pos/views/base_view.py
+++ tsa_pos/views/base_view.py
@@ -199,11 +199,7 @@ class BaseViews(object):
 
         arg = kwargs and kwargs or {}
         arg.update(url=self.list_url, col_defs=self.list_col_defs,
-<<<<<<< HEAD
-                #    cols=self.list_cols, 
-=======
->>>>>>> 84c5a9a (update invoice)
-                   buttons=self.list_buttons)
+        buttons=self.list_buttons)
         return arg
 
 
