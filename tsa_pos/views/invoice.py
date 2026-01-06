a0d516e2e5fac11dbf71305268fe6357475369a8
aliefmrizal-oss <aliefmrizal@gmail.com>
2026-01-06 09:41:01 +0700

    invoice andthegenk

--- tsa_pos/views/invoice.py
+++ tsa_pos/views/invoice.py
@@ -1,32 +1,30 @@
 from deform import widget, Form
 import colander
-from deform.widget import SequenceWidget
-
-from tsa_pos.views import invoice_items
 from tsa_pos.widgets import tsa_widget
+
 from ..models import Partner, Product, Invoices, InvoiceItems
 from . import BaseViews
 from ..i18n import _
 
 
-class CreateSchema(colander.Schema):
+class ListSchema(colander.Schema):
     id = colander.SchemaNode(
-        colander.Integer(),
-        missing=colander.drop,
-        title="ID",
-        widget=widget.HiddenWidget(),
+        colander.Integer(), missing=colander.drop, widget=widget.HiddenWidget()
     )
+    name = colander.SchemaNode(colander.String())
+    code = colander.SchemaNode(colander.String())
+    amount = colander.SchemaNode(colander.Float())
+    partner_id = colander.SchemaNode(colander.Integer(), title="Partner")
 
+
+class CreateSchema(colander.Schema):
     name = colander.SchemaNode(
         colander.String(), validator=colander.Length(min=1, max=128)
     )
-
     code = colander.SchemaNode(
         colander.String(), validator=colander.Length(min=1, max=128)
     )
-
     amount = colander.SchemaNode(colander.Float())
-
     est_delivery = colander.SchemaNode(
         colander.String(), widget=tsa_widget.BootStrapDateInputWidget()
     )
@@ -43,32 +41,25 @@ class CreateSchema(colander.Schema):
 
 
 class ListSchema(colander.Schema):
-    id = colander.SchemaNode(
-        colander.Integer(),
-        missing=colander.drop,
-        title="ID",
-        widget=widget.HiddenWidget(),
-    )
-
+    id = colander.SchemaNode(colander.Integer())
     name = colander.SchemaNode(
         colander.String(), validator=colander.Length(min=1, max=128)
     )
-
     code = colander.SchemaNode(
         colander.String(), validator=colander.Length(min=1, max=128)
     )
     amount = colander.SchemaNode(colander.Float())
-
     est_delivery = colander.SchemaNode(
         colander.String(), widget=tsa_widget.BootStrapDateInputWidget()
     )
-
-    invoice_items = colander.SchemaNode(
+    invoice_date = colander.SchemaNode(
         colander.String(), widget=tsa_widget.BootStrapDateInputWidget()
     )
-
     partner_id = colander.SchemaNode(
         colander.Integer(), widget=widget.SelectWidget(values=[]), title="Partner"
+    )
+    partner_id = colander.SchemaNode(
+        colander.Integer(), widget=widget.SelectWidget(values=[])
     )
 
     def after_bind(self, schema, appstruct):
