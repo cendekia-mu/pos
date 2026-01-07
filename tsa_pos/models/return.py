066b5e7009a0c8de261535663b350d7e056f5f2f
Dzakwan123 <182151108@student.unsil.ac.id>
2026-01-07 12:00:06 +0700

    penambahan models retur

---
+++ tsa_pos/models/return.py
@@ -0,0 +1,56 @@
+from sqlalchemy import (
+    String,
+    Integer,
+    ForeignKey,
+    DateTime,
+    Float,
+    Column,
+)
+from sqlalchemy.orm import relationship, backref
+from .base import StandardModel, Base
+
+class ReturnCategory(StandardModel, Base):
+    __tablename__ = 'return_category'
+    name = Column(String(128), unique=True)
+
+class Returns(StandardModel, Base):
+    __tablename__ = 'returns'
+    
+    category_id = Column(ForeignKey(
+        'return_category.id', ondelete='RESTRICT'), nullable=False)
+    category = relationship(
+        'ReturnCategory', backref=backref('returns'))
+    
+    invoice_id = Column(ForeignKey(
+        'invoices.id', ondelete='RESTRICT'), nullable=True)
+    invoice = relationship('Invoices', backref=backref('returns'))
+    
+    partner_id = Column(ForeignKey(
+        'partner.id', ondelete='RESTRICT'), nullable=False)
+    partner = relationship('Partner', backref=backref('returns'))
+    
+    name = Column(String(128))
+    code = Column(String(128), unique=True)
+    return_date = Column(DateTime)
+    total_amount = Column(Float)
+    reason = Column(String(255))
+    
+    return_items = relationship('ReturnItems', back_populates='return_order')
+
+class ReturnItems(Base):
+    __tablename__ = 'return_items'
+    
+    return_id = Column(ForeignKey(
+        'returns.id', ondelete='CASCADE'), nullable=False, primary_key=True)
+    product_id = Column(ForeignKey(
+        'product.id', ondelete='CASCADE'), nullable=False, primary_key=True)
+    
+    qty = Column(Integer)
+    price = Column(Float)
+    amount = Column(Float)
+    
+
+    return_order = relationship('Returns', back_populates='return_items')
+    product = relationship('Product', backref=backref('return_items'))
+   
+    invoice_item_id = Column(ForeignKey('invoice_items.invoice_id'))
